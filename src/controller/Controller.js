b79ebdbda4b54980e42748aad52daa2cdcfc9a9f
useon <yoosk4986@naver.com>
2023-12-13 17:51:26 +0900

    feat: 코치의메뉴입력잘못된경우throw문예외발생,에러메시지출력,그부분부터다시입력받는기능

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,4 +1,5 @@
 import Coach from '../models/Coach.js';
+import InfoCoach from '../models/InfoCoach.js';
 import handlerErrorAndProceed from '../utils/handleErrorAndProceed.js';
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
@@ -9,7 +10,10 @@ class Controller {
   async progress() {
     OutputView.printWelcomeMessage();
     const coachNames = await handlerErrorAndProceed(Controller.setCoachNames);
-    const coachData = await Controller.setCantFood(coachNames);
+    const coachData = await handlerErrorAndProceed(
+      Controller.setCantFood,
+      coachNames,
+    );
     console.log(coachData);
   }
 
@@ -22,7 +26,7 @@ class Controller {
     const data = [];
     for (const name of coachNames) {
       const inputValue = await InputView.readCantFood(name);
-      data.push([name, inputValue]);
+      data.push(new InfoCoach(name, inputValue).result);
     }
     return data;
   }
---
+++ src/models/InfoCoach.js
@@ -0,0 +1,54 @@
+import { TYPE_MENU } from '../constants/Setting.js';
+
+class InfoCoach {
+  #coachData;
+  constructor(coachName, inputCantFood) {
+    console.log(coachName, inputCantFood);
+    this.#validate(inputCantFood);
+    this.#coachData = this.#makeInofList(coachName, inputCantFood);
+  }
+
+  #validate(inputCantFood) {
+    if (inputCantFood.includes(',')) {
+      const convertStrToArr = inputCantFood.split(',');
+      if (convertStrToArr.length > 2)
+        throw new Error('[ERROR] 못 먹는 메뉴는 최대 2개여야 합니다.');
+      convertStrToArr.forEach((cantFood) => {
+        const validCantFoodArr = [];
+        TYPE_MENU.forEach((menus, type) => {
+          if (menus.includes(cantFood)) {
+            validCantFoodArr.push(true);
+          }
+        });
+        if (validCantFoodArr.length === 0)
+          throw new Error(
+            '[ERROR] 못 먹는 메뉴는 메뉴판에 있는 메뉴여야 합니다.',
+          );
+      });
+    }
+    if (!inputCantFood.includes(',')) {
+      const validCantFoodArr = [];
+      TYPE_MENU.forEach((menus, type) => {
+        if (menus.includes(inputCantFood)) {
+          validCantFoodArr.push(true);
+        }
+      });
+      if (validCantFoodArr.length === 0)
+        throw new Error(
+          '[ERROR] 못 먹는 메뉴는 메뉴판에 있는 메뉴여야 합니다.',
+        );
+    }
+  }
+
+  #makeInofList(coachName, inputCantFood) {
+    if (inputCantFood.includes(','))
+      return [coachName, inputCantFood.split(',')];
+    if (!inputCantFood.includes(',')) return [coachName, [inputCantFood]];
+  }
+
+  get result() {
+    return this.#coachData;
+  }
+}
+
+export default InfoCoach;
