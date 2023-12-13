bad411433510fad16c30c5e29b0de41ac8b1653b
useon <yoosk4986@naver.com>
2023-12-13 16:30:59 +0900

    feat: 점심 메뉴 추천 프로그램을 시작하는 멘트를 출력하는 기능

---
+++ src/controller/Controller.js
@@ -0,0 +1,11 @@
+import OutputView from '../views/OutputView.js';
+
+class Controller {
+  constructor() {}
+
+  progress() {
+    OutputView.printWelcomeMessage();
+  }
+}
+
+export default Controller;
---
+++ src/views/OutputView.js
@@ -0,0 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printWelcomeMessage() {
+    Console.print('점심 메뉴 추천을 시작합니다.');
+  },
+};
+
+export default OutputView;
