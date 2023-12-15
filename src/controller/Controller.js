59483dd8f9dc66d05cead58933b5bec343bf22e0
useon <yoosk4986@naver.com>
2023-12-15 22:27:35 +0900

    feat: 각 코치별로 메뉴를 추천하는 기능

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,6 +1,7 @@
 import CantFood from '../models/CantFood.js';
 import Category from '../models/Category.js';
 import CoachName from '../models/CoachName.js';
+import Recommendator from '../models/Recommendator.js';
 import handlerErrorAndProceed from '../utils/handleErrorAndProceed.js';
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
@@ -11,6 +12,11 @@ class Controller {
     const categoryArr = new Category().result;
     const coachNameArr = await handlerErrorAndProceed(this.#setCoachNameArr);
     const cantFoodMap = await this.#setTotalCantFoodMap(coachNameArr);
+    const totalRecommendationMap = await this.#setTotalRecommendation(
+      categoryArr,
+      coachNameArr,
+      cantFoodMap,
+    );
   }
 
   async #setCoachNameArr() {
@@ -33,6 +39,19 @@ class Controller {
   async #setCantFoodArr(name) {
     const inputValue = await InputView.readCantFood(name);
     return new CantFood(inputValue).result;
+  }
+
+  async #setTotalRecommendation(categoryArr, coachNameArr, cantFoodMap) {
+    const totalRecommendationMap = new Map();
+    coachNameArr.forEach((coachName) => {
+      const recommendation = new Recommendator(
+        categoryArr,
+        coachName,
+        cantFoodMap.get(coachName),
+      ).result;
+      totalRecommendationMap.set(coachName, recommendation);
+    });
+    return totalRecommendationMap;
   }
 }
 
---
+++ src/models/Recommendator.js
@@ -0,0 +1,38 @@
+import { Random } from '@woowacourse/mission-utils';
+import { TYPE_MENU } from '../constants/Setting.js';
+
+class Recommendator {
+  #coachRecommendationArr;
+  constructor(categoryArr, coachName, cantFoodArr) {
+    this.#coachRecommendationArr = [];
+    const cantMenu = cantFoodArr;
+    this.#makeRecommedation(categoryArr, coachName, cantMenu);
+  }
+
+  #makeRecommedation(categoryArr, coachName, cantMenu) {
+    categoryArr.forEach((category) => {
+      const pickedMenu = this.#pickMenu(category, cantMenu);
+      this.#coachRecommendationArr.push(pickedMenu);
+    });
+  }
+
+  #pickMenu(category, cantMenu) {
+    const categoryIndexArr = Array(TYPE_MENU.get(category).length)
+      .fill(0)
+      .map((_, i) => i + 1);
+    const pickedIndex = Random.shuffle(categoryIndexArr)[0];
+    const pickedMenu = TYPE_MENU.get(category)[pickedIndex - 1];
+    if (cantMenu.includes(pickedMenu)) this.#pickMenu(category, cantMenu);
+    const duplicationCount = this.#coachRecommendationArr.filter(
+      (menu) => menu === pickedMenu,
+    ).length;
+    if (duplicationCount === 2) cantMenu.push(pickedMenu);
+    return pickedMenu;
+  }
+
+  get result() {
+    return this.#coachRecommendationArr;
+  }
+}
+
+export default Recommendator;
